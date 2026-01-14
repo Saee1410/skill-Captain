5f883450c36f7f13f6f064332414a75fcffc1869
Saee1410 <saivijaybhosale@gmail.com>
2026-01-14 22:27:49 +0530

    react on sk

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Clock from './clock.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, react</h1>
+      <p>This is a JSX example.</p>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
